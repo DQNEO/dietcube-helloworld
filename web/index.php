81d4291328d3a8e3020862915c9f288df791b588
DQNEO <dqneoo@gmail.com>
2016-01-17 14:13:34 +0900

    show hello world

--- web/index.php
+++ web/index.php
@@ -1,21 +1,39 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+namespace  {
+    require_once __DIR__ . '/../vendor/autoload.php';
 
-use Dietcube\Dispatcher;
-use Pimple\Container;
+    use Pimple\Container;
 
-class Application extends Dietcube\Application
+    class Application extends \Dietcube\Application
 {
     public function config(Container $c)
     {
     }
-}
+    }
 
-class Route implements Dietcube\RouteInterface
+    class Route implements \Dietcube\RouteInterface
 {
     public function definition(Container $c)
     {
+        return [
+            ['GET', '/', 'Example::hello'],
+        ];
+    }
+    }
+
+}
+
+namespace Controller {
+
+    class ExampleController extends \Dietcube\Controller
+{
+    public function hello()
+    {
+        return 'hello world';
+    }
     }
 }
 
-Dispatcher::invoke('Application', __DIR__ . '/..', 'development');
+namespace  {
+    \Dietcube\Dispatcher::invoke('Application', __DIR__ . '/..', 'development');
+}
